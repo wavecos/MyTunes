d0ae41ad65ea295f613b044bc5410405c2e7e930
onix <alfre.arias@gmail.com>
2015-05-14 17:10:56 -0400

    Initial Commit

---
+++ MyTunes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyTunes
+//
+//  Created by onix on 5/14/15.
+//  Copyright (c) 2015 xiobit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
